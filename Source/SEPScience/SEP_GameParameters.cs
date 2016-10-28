06a8e5a2f2102eea113b645a6ae89bad77c1d895
DMagic <david.grandy@gmail.com>
2016-10-28 17:10:25 -0400

    Fix scale setting

--- Source/SEPScience/SEP_GameParameters.cs
+++ Source/SEPScience/SEP_GameParameters.cs
@@ -11,8 +11,8 @@ namespace SEPScience
 		public bool showAllVessels;
 		[GameParameters.CustomParameterUI("Window Fade Out", toolTip = "This controls whether the window fades out when not in focus", autoPersistance = true)]
 		public bool fadeOut = true;
-		[GameParameters.CustomFloatParameterUI("Scale", minValue = 50, maxValue = 200, asPercentage = true, displayFormat = "N0", stepCount = 5, autoPersistance = true)]
-		public float scale;
+		[GameParameters.CustomFloatParameterUI("Scale", minValue = 0.5f, maxValue = 2, asPercentage = true, displayFormat = "P0", autoPersistance = true)]
+		public float scale = 1;
 		[GameParameters.CustomParameterUI("Use As Default", autoPersistance = false)]
 		public bool useAsDefault;
 
